16fca371a294395c57de1a59f0e8e64970773ab1
Guzmán <guzmanbermejorodriguez@gmail.com>
2021-06-21 17:30:42 +0200

    Add top navigation bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <Nuxt />
+  <div id="wrapper" class="bg-secondary">
+    <header class="bg-primary">
+      <div class="navbar">
+        <nav class="navbar-section">
+          <a class="navbar-brand text-light text-bold mr-2" href="/"
+            >🦸 Hero or not 🦹</a
+          >
+        </nav>
+      </div>
+    </header>
+    <div id="content">
+      <Nuxt />
+    </div>
   </div>
 </template>
 
@@ -10,5 +21,26 @@
 *::after {
   box-sizing: border-box;
   margin: 0;
+}
+
+#wrapper > * {
+  margin: 0 auto;
+  max-width: 960px;
+}
+
+/* @media (min-width: 960px) {
+  #wrapper > * {
+    border: 1px solid #bcc3ce;
+    border-width: 0 1px;
+  }
+} */
+
+.navbar {
+  padding: 1rem 2rem;
+}
+#content {
+  background: white;
+  padding-top: 2rem;
+  width: 100%;
 }
 </style>
